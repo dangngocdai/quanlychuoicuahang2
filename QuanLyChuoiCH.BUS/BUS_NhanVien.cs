67e4ce48511f0dc67d92060f141c67eb7924a160
tuan <natsat123@gmail.com>
2019-02-25 13:32:41 +0700

    dal nv

---
+++ QuanLyChuoiCH.BUS/BUS_NhanVien.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using QuanLyChuoiCH.DAL;
+
+namespace QuanLyChuoiCH.BUS
+{
+    public class BUS_NhanVien
+    {
+    }
+}
