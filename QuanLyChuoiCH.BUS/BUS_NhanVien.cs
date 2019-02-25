ca112b0cef1a57eeee913f3f13d4c5e92b18915d
tuan <natsat123@gmail.com>
2019-02-25 13:51:54 +0700

    bus dal cthdban

--- QuanLyChuoiCH.BUS/BUS_NhanVien.cs
+++ QuanLyChuoiCH.BUS/BUS_NhanVien.cs
@@ -10,5 +10,25 @@ namespace QuanLyChuoiCH.BUS
 {
     public class BUS_NhanVien
     {
+        DAL_NhanVien e = new DAL_NhanVien();
+        public DataTable GetData()
+        {
+
+            return e.GetData();
+        }
+        public void insertData(String MaNV, String TenNV, String MaCH, String ChucVu, String NgayCong, String SDTNV, String MaNgQuanLy)
+        {
+
+            e.insertData(MaNV, TenNV, MaCH, ChucVu, NgayCong, SDTNV, MaNgQuanLy);
+        }
+        public void update(String MaNV, String TenNV, String MaCH, String ChucVu, String NgayCong, String SDTNV, String MaNgQuanLy)
+        {
+
+            e.update(MaNV, TenNV, MaCH, ChucVu, NgayCong, SDTNV, MaNgQuanLy);
+        }
+        public void delete(String MaNV)
+        {
+            e.delete(MaNV);
+        }
     }
 }
