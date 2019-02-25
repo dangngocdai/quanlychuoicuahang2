a03d323097736d07a5098542a247a74b763d4597
tuan <natsat123@gmail.com>
2019-02-25 14:54:08 +0700

    dal bus HDNhapKho

---
+++ QuanLyChuoiCH.BUS/BUS_HDNhapKho.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using QuanLyChuoiCH.DAL;
+
+namespace QuanLyChuoiCH.BUS
+{
+    public class BUS_HDNhapKho
+    {
+        DAL_HDNhapKho x = new DAL_HDNhapKho();
+        public DataTable GetData()
+        {
+
+            return x.GetData();
+        }
+        public void insertData(String MaHDNhapKho, String MaKho, String NgayNhap, String MaNCC, String MaNgQuanLy)
+        {
+
+            x.insertData(MaHDNhapKho, MaKho, NgayNhap, MaNCC, MaNgQuanLy);
+        }
+        public void update(String MaHDNhapKho, String MaKho, String NgayNhap, String MaNCC, String MaNgQuanLy)
+        {
+
+            x.update(MaHDNhapKho, MaKho, NgayNhap, MaNCC, MaNgQuanLy);
+
+        }
+        public void delete(String MaHDNhapKho)
+        {
+            x.delete(MaHDNhapKho);
+        }
+
+    }
+}
---
+++ QuanLyChuoiCH.DAL/DAL_HDNhapKho.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+
+namespace QuanLyChuoiCH.DAL
+{
+   public class DAL_HDNhapKho : DBConnect
+    {
+        public DataTable GetData()
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("HDNhapKho_Lay", con);//thực hiện các câu lệnh truy vẫn. nhưng đéo có truy vẫn mà là store procedure
+            cmd.CommandType = CommandType.StoredProcedure;//chú thích cho hiểu là đang dùng storedprocedure
+            DataTable tb = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(tb);
+            con.Close();
+            return tb;
+        }
+        public void insertData(String MaHDNhapKho, String MaKho, String NgayNhap, String MaNCC, String MaNgQuanLy)
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("HDNhapKho_Them", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.Add(new SqlParameter("@MaHDNhapKho", MaHDNhapKho));
+            cmd.Parameters.Add(new SqlParameter("@MaKho", MaKho));
+            cmd.Parameters.Add(new SqlParameter("@NgayNhap", NgayNhap));
+            cmd.Parameters.Add(new SqlParameter("@MaNCC", MaNCC));
+            cmd.Parameters.Add(new SqlParameter("@MaNgQuanLy", MaNgQuanLy));
+            cmd.ExecuteNonQuery();
+            con.Close();
+        }
+        public void update(String MaHDNhapKho, String MaKho, String NgayNhap, String MaNCC, String MaNgQuanLy)
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("HDNhapKho_Them", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.Add(new SqlParameter("@MaHDNhapKho", MaHDNhapKho));
+            cmd.Parameters.Add(new SqlParameter("@MaKho", MaKho));
+            cmd.Parameters.Add(new SqlParameter("@NgayNhap", NgayNhap));
+            cmd.Parameters.Add(new SqlParameter("@MaNCC", MaNCC));
+            cmd.Parameters.Add(new SqlParameter("@MaNgQuanLy", MaNgQuanLy));
+            cmd.ExecuteNonQuery();
+            con.Close();
+        }
+        public void delete(String MaHDNhapKho)
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("HDNhapKho_Xoa", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.Add(new SqlParameter("@MaHDNhapKho", MaHDNhapKho));
+            cmd.ExecuteNonQuery();
+            con.Close();
+        }
+    }
+}
